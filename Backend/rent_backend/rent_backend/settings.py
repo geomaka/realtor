ef4c7a99654c650c2104a37ecd4c9ef93cc000e7
George Maina <107925640+geomaka@users.noreply.github.com>
2024-07-28 00:41:58 +0300

    Changes to the allow hosts

--- Backend/rent_backend/rent_backend/settings.py
+++ Backend/rent_backend/rent_backend/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://realtor-1-kllo.onrender.com']
+ALLOWED_HOSTS = ['realtor-1-kllo.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
